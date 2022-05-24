d110eba83571fc6609227e29b36db24bfcd39cb1
Aisylu24 <firnatovna@mail.ru>
2022-05-24 16:51:19 +0300

    16 useReducer

--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,50 +1,36 @@
-import React, {useState} from "react";
+import React, {useReducer} from "react";
+import {reducer, TOGGLE_CONSTANT} from "./Reducer";
 
 
 type AccPropsType={
     title: string
-   // collapsed: boolean
 }
 
-export function UncontrolledAccordion(props:AccPropsType) {
+export function UncontrolledAccordion(props: AccPropsType) {
 
-    let [collapsed, setCol] = useState(false)
+    let [state, dispatch] = useReducer(reducer, {collapsed: false})
 
-        return <>
-            <AccTitle name={props.title} onClick={()=>(setCol(!collapsed))}/>
-            {/*<button onClick={()=>(setCol(!collapsed))}>Свернуть</button>*/}
-                { !collapsed && <AccBody/>}
-            </>
-    }
-
-
-
-// export function Accordion(props:AccPropsType) {
-//     if (props.collapsed) {
-//     return(
-//         <><AccTitle name={props.title}/></> )}
-//     else {
-//         return (
-//         <><AccTitle name={props.title}/>
-//         <AccBody/>  </>)
-//        }}
-
+    return <>
+        <AccTitle name={props.title} onClick={() => (dispatch({type: TOGGLE_CONSTANT}))}/>
+        {!state.collapsed && <AccBody/>}
+    </>
+}
 
 type AccTitleType = {
-    name:string
+    name: string
     onClick: () => void
 }
 
-function AccTitle(props: AccTitleType){
+function AccTitle(props: AccTitleType) {
     console.log("AccT")
-    return <h3 onClick={()=>props.onClick()}>{props.name}</h3>
-    }
+    return <h3 onClick={() => props.onClick()}>{props.name}</h3>
+}
 
 
-function AccBody(){
+function AccBody() {
     console.log("AccB")
     return <ul>
-            <li>hard</li>
-            <li>work</li>
-        </ul>
-    }
+        <li>hard</li>
+        <li>work</li>
+    </ul>
+}
