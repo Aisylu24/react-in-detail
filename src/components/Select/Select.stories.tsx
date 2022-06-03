84d1fe190a7d5869a2f62051d004a2d768015ab4
Aisylu24 <firnatovna@mail.ru>
2022-06-03 17:20:53 +0300

    18 WithCheckbox

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react'
+import React, {ChangeEvent, useMemo, useState} from 'react'
 import {Select} from "./Select";
 
 export default {
@@ -31,16 +31,15 @@ export const WithoutValue = () => {
 }
 
 
-
 export const WithMemo = () => {
     const [value, setValue] = useState(null)
     const [items, setItems] = useState([
         {title: "Kazan", value: '1', region: 16, isTheMillionCity: true, isCapital: true},
-        {title: "Laishevo", value: '2', region: 16, isTheMillionCity:false, isCapital: false},
-        {title: "Ufa", value: '3', region: 2, isTheMillionCity:true, isCapital: true},
-        {title: "Sterlitamak", value: '4', region: 2, isTheMillionCity:false, isCapital: false},
-        {title: "Samara", value: '5', region: 63, isTheMillionCity:true,isCapital: true},
-        {title: "Tolyatti", value: '6', region: 63, isTheMillionCity:false, isCapital: false},
+        {title: "Laishevo", value: '2', region: 16, isTheMillionCity: false, isCapital: false},
+        {title: "Ufa", value: '3', region: 2, isTheMillionCity: true, isCapital: true},
+        {title: "Sterlitamak", value: '4', region: 2, isTheMillionCity: false, isCapital: false},
+        {title: "Samara", value: '5', region: 63, isTheMillionCity: true, isCapital: true},
+        {title: "Tolyatti", value: '6', region: 63, isTheMillionCity: false, isCapital: false},
     ])
 
     const capitalOfRegions = useMemo(() => {
@@ -80,5 +79,76 @@ export const WithMemo = () => {
     </>
 }
 
+
+export const WithCheckbox = () => {
+    const [value, setValue] = useState(null)
+    const [items, setItems] = useState([
+        {title: "Kazan", value: '1', region: 16, isTheMillionCity: true, isCapital: true},
+        {title: "Laishevo", value: '2', region: 16, isTheMillionCity: false, isCapital: false},
+        {title: "Ufa", value: '3', region: 2, isTheMillionCity: true, isCapital: true},
+        {title: "Sterlitamak", value: '4', region: 2, isTheMillionCity: false, isCapital: false},
+        {title: "Samara", value: '5', region: 63, isTheMillionCity: true, isCapital: true},
+        {title: "Tolyatti", value: '6', region: 63, isTheMillionCity: false, isCapital: false},
+    ])
+
+
+    const [filter, setFilter] = useState({
+        capitals: false,
+        tatarstan: false,
+        towns: false
+    })
+
+
+    let filteredCities = items
+
+    const capitalOfRegions = useMemo(() => {
+        console.log(1)
+        return items.filter((city) => city.isCapital)
+    }, [items])
+
+    const citiesOfTatarstan = useMemo(() => {
+        console.log(2)
+        return items.filter((city) => city.region === 16)
+    }, [items])
+
+    const notTheMillionCity = useMemo(() => {
+        console.log(3)
+        return items.filter((city) => !city.isTheMillionCity)
+    }, [items])
+
+
+    if(filter.capitals) {
+        filteredCities = capitalOfRegions
+    }
+    if(filter.tatarstan)
+        filteredCities = citiesOfTatarstan
+    if(filter.towns)
+        filteredCities = notTheMillionCity
+
+    return <>
+        <div>
+            <Select
+                value={value}
+                onChange={setValue}
+                items={filteredCities}/>
+        </div>
+        <fieldset>
+            <legend>Choose filter:</legend>
+            <div>
+                <input type="checkbox" id="Capitals" name='Capitals' checked={filter.capitals} onChange={() => setFilter({...filter, capitals: !filter.capitals})} disabled={filter.tatarstan || filter.towns}/>
+                <label htmlFor="Capitals">Capitals</label>
+            </div>
+            <div>
+                <input type="checkbox" id="Tatarstan" name="Tatarstan" checked={filter.tatarstan} onChange={() => setFilter({...filter, tatarstan: !filter.tatarstan})} disabled={filter.capitals || filter.towns}/>
+                <label htmlFor="Tatarstan">Tatarstan's cities</label>
+            </div>
+            <div>
+                <input type="checkbox" id="towns" name="towns" checked={filter.towns} onChange={() => setFilter({...filter, towns:!filter.towns})} disabled={filter.capitals || filter.tatarstan}/>
+                <label htmlFor="towns">Small towns</label>
+            </div>
+
+        </fieldset>
+    </>
+}
 
 
