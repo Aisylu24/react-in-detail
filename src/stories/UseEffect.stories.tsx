d11c9e78cfeb4c50f7106a838eaef9a7e0474692
Aisylu24 <firnatovna@mail.ru>
2022-07-01 21:56:43 +0300

    22

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -8,30 +8,69 @@ export default {
 
 export const SimpleExample = () => {
 
-const [fake, setFake] = useState<number>(1)
+    const [fake, setFake] = useState<number>(1)
     const [counter, setCounter] = useState<number>(1)
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log('useEffect will work after every render component')
         document.title = counter.toString()
     })
 
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log('useEffect only first render (componentDidMount)')
         document.title = counter.toString()
-    },[counter])
+    }, [counter])
 
 
-    useEffect(()=> {
+    useEffect(() => {
         console.log('useEffect first render and every counter change')
         document.title = counter.toString()
-    },[counter])
+    }, [counter])
     return <>
 
-        <button onClick={() => setFake(fake +1)}>+</button>
-        <button onClick={() => setCounter(counter+1)}>counter+</button>
-       hello,  {counter} {fake}
+        <button onClick={() => setFake(fake + 1)}>+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+        hello, {counter} {fake}
+
+    </>
+}
+
+
+export const SetTimeoutExample = () => {
+
+    const [fake, setFake] = useState<number>(1)
+    const [counter, setCounter] = useState<number>(1)
+
+    useEffect(() => {
+        setTimeout(() => {
+            document.title = counter.toString()
+        }, 1000)
+    }, [counter]) // props.counter
+
+
+    return <>
+
+        <button onClick={() => setFake(fake + 1)}>+</button>
+        <button onClick={() => setCounter(counter + 1)}>counter+</button>
+        hello, {counter} {fake}
+
+    </>
+}
+
+export const SetIntervalExample = () => {
+
+    const [fake, setFake] = useState<number>(1)
+    const [counter, setCounter] = useState<number>(1)
+
+    useEffect(() => {
+        // setCounter(counter+1) // будет всегда два
+        setCounter((state) => state + 1) //
+    }, [])
+
+
+    return <>
+        hello, counter : {counter}, fake: {fake}
 
     </>
 }
